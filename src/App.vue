b53652ea07251dca73e0b5d3adc7b9f86b0dd968
DamianoCasolari <damianocasolari@gmail.com>
2023-05-02 11:26:52 +0200

    add splash page

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ import SiteFooter from './components/SiteFooter.vue';
 export default {
   data() {
     return {
-
+      LoadingE: true
     }
   },
   components: {
@@ -16,6 +16,14 @@ export default {
     SiteMain,
     SiteFooter
   },
+  methods: {
+
+    loadingPage() {
+      setTimeout(() => {
+        this.LoadingE = false
+      }, 1500)
+    }
+  },
   mounted() {
     const anchorTags = document.querySelectorAll('a[href="#"]');
     anchorTags.forEach(a => {
@@ -23,19 +31,50 @@ export default {
         event.preventDefault();
       });
     });
-  }
 
+    this.loadingPage()
+  }
 }
 
 </script>
 
 <template>
-  <div v-cloak>
-    <CountdownEvent />
-    <SiteHeader />
-    <SiteMain />
-    <SiteFooter />
+  <div>
+    <div v-if="LoadingE">
+      <div class="logo_container">
+        <img class="splash_page" src="./assets/img/images/dark-logo.png" alt="Main_logo">
+      </div>
+    </div>
+    <div v-show="!LoadingE">
+      <CountdownEvent />
+      <SiteHeader />
+      <SiteMain />
+      <SiteFooter />
+    </div>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.logo_container {
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .splash_page {
+    opacity: 1;
+    animation: wakeup 1.5s linear both;
+  }
+
+}
+
+@keyframes wakeup {
+  from {
+    opacity: 0;
+  }
+
+  to {
+    opacity: 1;
+  }
+}
+</style>
